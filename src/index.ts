38c26d879a6479539161f5e161f8243c34119fb9
julesbsz <j.bousrez@outlook.com>
2025-03-07 12:21:48 +0100

    feat: added login & register routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import express from "express";
+import authRoutes from "./routes/auth.routes";
+import client from "./config/db";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", async (req, res) => {
 	res.json({ message: "API REST avec Express en TypeScript" });
+});
+
+app.get("/test", async (req, res) => {
+	try {
+		const result = await client.query("SELECT NOW()");
+		res.json({ message: "Connexion réussie", time: result.rows[0] });
+	} catch (error) {
+		console.error("Erreur API :", error);
+		res.status(500).json({ error: "Erreur de connexion" });
+	}
 });
 
 // Démarrage du serveur
